ecdede3fee578cc4d098c6ae140d936e7849d7a8
Gustavo Almeida <gustavo@cuideme.care>
2021-02-01 18:58:06 -0300

    Criação da rota de listar services para providers

--- src/routes/services.routes.ts
+++ src/routes/services.routes.ts
@@ -1,14 +1,24 @@
 /* eslint-disable camelcase */
 import { Router } from 'express';
+import { getRepository } from 'typeorm';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import ProvidedService from '../models/ProvidedService';
 
 import CreateProvidedServiceService from '../services/CreateProvidedServiceService';
 
 const providersRouter = Router();
 
+providersRouter.get('/', ensureAuthenticated, async (request, response) => {
+  const serviceRepository = getRepository(ProvidedService);
+
+  const services = await serviceRepository.find({
+    where: { provider_id: request.user.id },
+  });
+
+  return response.json(services);
+});
 providersRouter.post('/', ensureAuthenticated, async (request, response) => {
   const { name, description, value, type, dueDate } = request.body;
-  console.log(request.user.id);
 
   const createService = new CreateProvidedServiceService();
 
