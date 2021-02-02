e5f21ab2618326ae82c317054651851a712f13f8
Gustavo Almeida <gustavo@cuideme.care>
2021-02-02 00:52:40 -0300

    Filtragem de Serviços na rota recebendo DD/MM/YYYY

--- src/routes/services.routes.ts
+++ src/routes/services.routes.ts
@@ -1,6 +1,7 @@
 /* eslint-disable camelcase */
 import { Router } from 'express';
 import { getRepository } from 'typeorm';
+import moment from 'moment';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import ProvidedService from '../models/ProvidedService';
 
@@ -9,14 +10,41 @@ import CreateProvidedServiceService from '../services/CreateProvidedServiceServi
 const providersRouter = Router();
 
 providersRouter.get('/', ensureAuthenticated, async (request, response) => {
+  let services = [];
   const serviceRepository = getRepository(ProvidedService);
 
-  const services = await serviceRepository.find({
+  const { startDate, endDate } = request.query;
+
+  let parsedStartDate = String(startDate);
+  let parsedEndDate = String(endDate);
+  let parsedDueDate = '';
+
+  services = await serviceRepository.find({
     where: { provider_id: request.user.id },
   });
 
+  if (parsedStartDate && parsedEndDate) {
+    parsedStartDate = parsedStartDate.split('/').reverse().join('-');
+    parsedEndDate = parsedEndDate.split('/').reverse().join('-');
+    parsedStartDate = moment(parsedStartDate).format('YYYYMMDD');
+    parsedEndDate = moment(parsedEndDate).format('YYYYMMDD');
+
+    services = services.filter(service => {
+      parsedDueDate = moment(service.due_date).format('YYYYMMDD');
+      if (
+        Number(moment(service.created_at).format('YYYYMMDD')) >=
+          Number(parsedStartDate) &&
+        Number(moment(parsedDueDate).format('YYYYMMDD')) <=
+          Number(parsedEndDate)
+      ) {
+        return true;
+      }
+      return false;
+    });
+  }
   return response.json(services);
 });
+
 providersRouter.post('/', ensureAuthenticated, async (request, response) => {
   const { name, description, value, type, dueDate } = request.body;
 
