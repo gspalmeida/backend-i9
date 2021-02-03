cb15cf2a94b77af22fc1e58320bc04839aa4dc00
Gustavo Almeida <gustavo@cuideme.care>
2021-02-03 10:13:09 -0300

    Autenticação de admin pronta

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -2,29 +2,58 @@
 import { Router } from 'express';
 
 import AuthenticateProviderService from '../services/AuthenticateProviderService';
+import AuthenticateAdminService from '../services/AuthenticateAdminService';
 
 interface ProviderWithoutPassword {
   email: string;
   password?: string;
 }
+interface AdminWithoutPassword {
+  email: string;
+  password?: string;
+}
+interface ResponseData {
+  admin?: AdminWithoutPassword;
+  provider?: ProviderWithoutPassword;
+  token: string;
+}
 
 const authRouter = Router();
 
 authRouter.post('/', async (request, response) => {
+  let responseData: ResponseData = {
+    admin: {} as AdminWithoutPassword,
+    provider: {} as ProviderWithoutPassword,
+    token: '',
+  };
   const { email, password } = request.body;
-
   const authenticateProvider = new AuthenticateProviderService();
+  const authenticateAdmin = new AuthenticateAdminService();
 
-  const { provider, token } = await authenticateProvider.execute({
+  const { admin, token: adminToken } = await authenticateAdmin.execute({
     email,
     password,
   });
-
-  const parsedProvider: ProviderWithoutPassword = provider;
-
-  delete parsedProvider.password;
-
-  return response.json({ parsedProvider, token });
+  if (admin) {
+    console.log('entrou');
+    const parsedAdmin: AdminWithoutPassword = admin;
+    delete parsedAdmin.password;
+    responseData = { admin: parsedAdmin, token: adminToken };
+  }
+  if (admin.email === undefined) {
+    const {
+      provider,
+      token: providerToken,
+    } = await authenticateProvider.execute({
+      email,
+      password,
+    });
+    const parsedProvider: ProviderWithoutPassword = provider;
+    delete parsedProvider.password;
+    responseData = { provider: parsedProvider, token: providerToken };
+    return response.json(responseData);
+  }
+  return response.json(responseData);
 });
 
 export default authRouter;
