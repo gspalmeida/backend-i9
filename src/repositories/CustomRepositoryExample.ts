2acbe108e4ce488d9d344f0836e158f1bd73f4b1
Gustavo Almeida <gustavo@cuideme.care>
2021-02-04 14:55:36 -0300

    Remoção do arquivo de exemplo que estava causando erro no build;

--- src/repositories/CustomRepositoryExample.ts
+++ src/repositories/CustomRepositoryExample.ts
@@ -1,16 +1,16 @@
-import { EntityRepository, Repository } from 'typeorm';
-/* Importar o Model que irá criar o método */
+/* import { EntityRepository, Repository } from 'typeorm';
+Importar o Model que irá criar o método
 import Caregiver from '../models/Caregiver';
 
 @EntityRepository(Caregiver)
 class CaregiverRepository extends Repository<Caregiver> {
-  /*
+
     Criação do Método Customizado de Exemplo (que retornaria as notificações
     não lidas dos ageds cuidados por esse caregiver);
 
     Esse metodo receberá "anyParameter: boolean" que terá que ser passado lá
     nos services que forem chamar ele
-  */
+
   public async findAgedsUnreadNotifications(
     anyParameter: boolean,
   ): Promise<Caregiver | null> {
@@ -21,11 +21,12 @@ class CaregiverRepository extends Repository<Caregiver> {
     return agedIds || null;
   }
 }
-/*
+
   O repositório customizado será importado no service que o utilizar com o
   "getCustomRepositoy(CaregiverRepository)" do typeOrm, permitindo ele usar
   todos os operadores de dados do typeOrm + o "findAgedsUnreadNotification"
   aqui criado
-*/
+
 
 export default CaregiverRepository;
+*/
